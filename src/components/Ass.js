a9d95e730379144a6929893e60e6528008c40e08
nikunj gupta <59887608+nikunj2001@users.noreply.github.com>
2022-06-03 15:36:23 +0530

    assignment

---
+++ src/components/Ass.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import "./style.css"
+const Ass = () => {
+    return (
+        <div>
+            <div className="header">
+                <h2>Full Stack Developer</h2>
+            </div>
+            <div className="body">
+                <div className="left">
+                    <div className="images">
+                        <div className="img-1">
+                            <img src="./images/html-5.png" alt="" />
+                        </div>
+                        <div className="img-2">
+                            <img src="./images/css-3.png" alt="" />
+                        </div>
+                        <div className="img-3">
+                            <img src="./images/java-script.png" alt="" />
+
+                        </div>
+                        <div className="img-4">
+                            <img src="./images/angular.jpg" alt="" />
+                        </div>
+                        <div className="img-5">
+                            <img src="./images/angular.jpg" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className="center">
+                    <div className="left-stick">
+                        <div className="s s-1"></div>
+                        <div className="s s-2"></div>
+                        <div className="s s-3"></div>
+                        <div className="s s-4"></div>
+                        <div className="s s-5"></div>
+                    </div>
+                    <div className="center-img">
+                        <img src="./images/laptop.png" alt="" />
+                    </div>
+
+                    <div className="right-stick">
+                        <div className="sr s-6"></div>
+                        <div className="sr s-7"></div>
+                        <div className="sr s-8"></div>
+                        <div className="sr s-9"></div>
+                        <div className="sr s-10"></div>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="images">
+                        <div className="img-1">
+                            <img src="./images/php.png" alt="" />
+
+                        </div>
+                        <div className="img-2">
+                            <img src="./images/ruby.png" alt="" />
+
+                        </div>
+                        <div className="img-3">
+                            <img src="./images/python.png" alt="" />
+
+                        </div>
+                        <div className="img-4">
+                            <img src="./images/java.png" alt="" />
+
+                        </div>
+                        <div className="img-5">
+                            <img src="./images/github.png" alt="" />
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Ass
